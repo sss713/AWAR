fa1dac7211bc1bcaa5a68d53499576b9207b1a8b
Mikhail Alikevich <malikevich@gmail.com>
2023-02-16 15:33:31 +0300

    Chenged app theme

--- app/src/main/java/com/example/awar/MainActivity.java
+++ app/src/main/java/com/example/awar/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.awar;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.view.Window;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -29,8 +32,17 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_library, R.id.navigation_home, R.id.navigation_userprofile, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //Night Theme Defualt
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        //Night Theme Defualt
+
+        //Disable bottom navigation bar
+        Window w = getWindow();
+        w.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        //Disable bottom navigation bar
     }
 
 }
